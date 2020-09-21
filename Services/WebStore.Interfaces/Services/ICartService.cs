341bae8e7cc63c55f4f652a2fb34d0335b56e918
andrey5553 <andrey5553@yandex.ru>
2020-09-21 12:32:32 +0300

    Перенос интерфейсов в отдельный проект

---
+++ Services/WebStore.Interfaces/Services/ICartService.cs
@@ -0,0 +1,15 @@
+﻿using WebStore.Domain.ViewModels;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface ICartService
+    {
+        void DecrementFromCart(int id);
+        void RemoveFromCart(int id);
+        void RemoveAll();
+        void AddToCart(int id);
+
+        CartViewModel TransformCart();
+    }
+
+}
---
+++ Services/WebStore.Interfaces/Services/IOrdersService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface IOrdersService
+    {
+        IEnumerable<Order> GetUserOrders(string userName);
+        Order GetOrderById(int id);
+        Order CreateOrder(OrderViewModel orderModel, CartViewModel transformCart, string userName);
+    }
+}
