781f8e2d4e882687509f0d498b4c81bab76d7cd9
andrey5553 <andrey5553@yandex.ru>
2020-09-21 13:13:36 +0300

    Тестовое WebApi

---
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly List<string> _values =
+            Enumerable
+            .Range(1, 10)
+            .Select(i => $"value-{i}")
+            .ToList();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() => _values;
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+            if (id >= _values.Count)
+            {
+                return NotFound();
+            }
+            return _values[id];
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, string value)
+        {
+            if (id < 0 || id >= _values.Count)
+                return BadRequest();
+
+            _values[id] = value;
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Post(string value)
+        {
+            _values.Add(value);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= _values.Count)
+                return NotFound();
+
+            _values.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
